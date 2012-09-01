e560e35b0deb3c9dbd9bc47eee67371524642b81
hanson <hangzhong.yu@gmail.com>
2012-09-02 00:32:08 +0800

    redesign the vlist and implement the init

--- datastructures/lists/vlist.c
+++ datastructures/lists/vlist.c
@@ -34,16 +34,77 @@ typedef struct BLOCK
 {
     struct BLOCK *pre;
     struct BLOCK *next;
-    int *elem;
+    int *elems;
 }vlistBlock;
 
-void vlistInit(vlistBlock *vb, int size)
+void vlistInit(vlistBlock *head, int size)
 {
+    vlistBlock *cur;
+    int cursize = 1;
+    while (size > 0)
+    {
+        cur = (vlistBlock *)malloc(sizeof(vlistBlock));
+        cur->elems = (int *)malloc(sizeof(cursize));
+        
+        cur->pre = NULL;
+        cur->next = NULL;
+        if (size == 1)
+        {
+            head = cur;
+        }
+        else
+        {
+            head->pre = cur;
+            cur->next = head;
+
+            head = cur;
+        }
+
+        size -= cursize;
+        cursize *= 2;
+    }
 }
+
+/* remove the element at the position index.
+ * @param elem
+ *      the index at which to remove the element.
+ * */
+void removeFromVlist(int index)
+{}
+
+/*reset the element at the position index.
+ * @param elem
+ *      the index at which to reset the element.
+ * */
+void set(int index, int value)
+{}
+
+/*get the element at the position index.
+ * @param elem
+ *      the index at which to look up the element.
+ * @param elem
+ *      the element at the position.
+ * */
+int get(int index)
+{}
+
+/*add a new element to the end of the array.
+ * @param elem
+ *      the element to add.
+ * */
+void add(int value)
+{}
+
+/*return the size of the vlist.
+ * */
+int size()
+{}
 
 int main()
 {
-    vlistBlock vb;
-    //vlistInit(&vb, 1);
+    vlistBlock head;
+
+    vlistInit(&vb, 1);
     return 0;
 }
+
