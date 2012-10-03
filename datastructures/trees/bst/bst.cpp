d595fe4abf411ece8cf59d1275c47638dd4ad2a3
hanson <hangzhong.yu@gmail.com>
2012-10-04 00:05:24 +0800

    add bst with search iter/recur module

---
+++ datastructures/trees/bst/bst.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+//treenode
+typedef struct TREENODE
+{
+    TREENODE *leftChild;
+    TREENODE *rightChild;
+    int key;
+}TreeNode;
+
+//Searching
+//iterative process
+TreeNode *find_Iterative(TreeNode *root, int key)
+{
+    TreeNode *currentNode = root;
+    while (currentNode != NULL)
+    {
+        if (currentNode->key == key)
+        {
+            return currentNode;
+        }
+        else if (currentNode->key < key)
+        {
+            currentNode = currentNode->leftChild;
+        }
+        else
+        {
+            currentNode = currentNode->rightChild;
+        }
+    }
+
+    return currentNode;
+}
+
+TreeNode *find_Recusive(TreeNode* currentNode, int key)
+{
+    if (currentNode->key == key)
+    {
+        return currentNode;
+    }
+    else if (currentNode->key < key)
+    {
+        find_Recusive(currentNode->leftChild, key);
+    }
+    else
+    {
+        find_Recusive(currentNode->rightChild, key);
+    }
+}
+
+int main()
+{
+    TreeNode * root = (TreeNode *)malloc(sizeof(TreeNode));
+    
+    return 0;
+}
+
