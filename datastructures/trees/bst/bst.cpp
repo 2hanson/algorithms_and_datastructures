10f6b4c8b82556336413c7f3bd735d95264d08b6
hanson <hangzhong.yu@gmail.com>
2012-10-23 00:42:26 +0800

    build bst but delete dan traversal

--- datastructures/trees/bst/bst.cpp
+++ datastructures/trees/bst/bst.cpp
@@ -55,6 +55,82 @@ TreeNode *find_Recusive(TreeNode* currentNode, int key)
     }
 }
 
+void insertNode_Iterative(TreeNode* root, int data)
+{
+    if (root == NULL)
+    {
+        root = (TreeNode *)malloc(sizeof(TreeNode));
+        root->key = data;
+        root->leftChild = NULL;
+        root->rightChild = NULL;
+
+        return;
+    }
+
+    TreeNode *m_root = root;
+
+    while (m_root != NULL)
+    {
+        if (data = m_root->key)
+        {
+            return;
+        }
+        
+        if (data > m_root->key)
+        {
+            if (m_root->leftChild == NULL)
+            {
+                m_root->leftChild = (TreeNode *)malloc(sizeof(TreeNode));
+                m_root->leftChild->rightChild = m_root->leftChild->leftChild = NULL;
+                m_root->leftChild->key = data;
+
+                return;
+            }
+            else
+            {
+                m_root = m_root->leftChild;
+            }
+        }
+        else
+        {
+            if (m_root->rightChild == NULL)
+            {
+                m_root->rightChild = (TreeNode *)malloc(sizeof(TreeNode));
+                m_root->rightChild->rightChild = m_root->rightChild->leftChild = NULL;
+                m_root->rightChild->key = data;
+
+                return;
+            }
+            else
+            {
+                m_root = m_root->rightChild;
+            }
+        }
+    }
+
+}
+
+void insertNode_Recusive(TreeNode *treeNode, int data)
+{
+    if (treeNode == NULL)
+    {
+        TreeNode * newNode = (TreeNode *)malloc(sizeof(TreeNode));
+        newNode->key = data;
+        newNode->leftChild = NULL;
+        newNode->rightChild = NULL;
+
+        treeNode = newNode;
+    }
+    else if (treeNode->key < newNode->key)
+    {
+        insertNode(treeNode->leftChild, newNode);
+    }
+    else
+    {
+        insertNode(treeNode->rightChild, newNode);
+    }
+}
+
 int main()
 {
     TreeNode * root = (TreeNode *)malloc(sizeof(TreeNode));
