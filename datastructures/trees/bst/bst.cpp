d3766523cc813b6612f19a0de1780a19248d84ac
hanson <hangzhong.yu@gmail.com>
2012-10-23 10:07:57 +0800

    finish bst

--- datastructures/trees/bst/bst.cpp
+++ datastructures/trees/bst/bst.cpp
@@ -10,6 +10,7 @@ using namespace std;
 //treenode
 typedef struct TREENODE
 {
+    TREENODE *parent;
     TREENODE *leftChild;
     TREENODE *rightChild;
     int key;
@@ -26,7 +27,7 @@ TreeNode *find_Iterative(TreeNode *root, int key)
         {
             return currentNode;
         }
-        else if (currentNode->key < key)
+        else if (key < currentNode->key)
         {
             currentNode = currentNode->leftChild;
         }
@@ -45,7 +46,7 @@ TreeNode *find_Recusive(TreeNode* currentNode, int key)
     {
         return currentNode;
     }
-    else if (currentNode->key < key)
+    else if (key < currentNode->key)
     {
         find_Recusive(currentNode->leftChild, key);
     }
@@ -76,14 +77,14 @@ void insertNode_Iterative(TreeNode* root, int data)
             return;
         }
         
-        if (data > m_root->key)
+        if (data < m_root->key)
         {
             if (m_root->leftChild == NULL)
             {
                 m_root->leftChild = (TreeNode *)malloc(sizeof(TreeNode));
                 m_root->leftChild->rightChild = m_root->leftChild->leftChild = NULL;
                 m_root->leftChild->key = data;
-
+                m_root->leftChild->parent = m_root->leftChild;
                 return;
             }
             else
@@ -98,7 +99,7 @@ void insertNode_Iterative(TreeNode* root, int data)
                 m_root->rightChild = (TreeNode *)malloc(sizeof(TreeNode));
                 m_root->rightChild->rightChild = m_root->rightChild->leftChild = NULL;
                 m_root->rightChild->key = data;
-
+                m_root->rightChild->parent = m_root->rightChild;
                 return;
             }
             else
@@ -110,7 +111,7 @@ void insertNode_Iterative(TreeNode* root, int data)
 
 }
 
-void insertNode_Recusive(TreeNode *treeNode, int data)
+void insertNode_Recusive(TreeNode *treeNode, TreeNode* parent, int data)
 {
     if (treeNode == NULL)
     {
@@ -118,17 +119,101 @@ void insertNode_Recusive(TreeNode *treeNode, int data)
         newNode->key = data;
         newNode->leftChild = NULL;
         newNode->rightChild = NULL;
-
+        newNode->parent = parent;
         treeNode = newNode;
     }
-    else if (treeNode->key < newNode->key)
+    else if (newNode->key < treeNode->key)
     {
-        insertNode(treeNode->leftChild, newNode);
+        insertNode(treeNode->leftChild, treeNode, newNode);
     }
     else
     {
-        insertNode(treeNode->rightChild, newNode);
+        insertNode(treeNode->rightChild, TreeNode, newNode);
     }
+}
+
+//find the smallest node that is a child of curNode
+TreeNode* getSuccessor(TreeNode* curNode)
+{
+    TreeNode* tempNode = curNode;
+    while (tempNode->leftChild)
+    {
+        tempNode = tempNode->leftChild;
+    }
+
+    return tempNode;
+}
+
+//remove the reference to curNode from curNode->parent and 
+//replace it with newNode.
+void replaceNodeInParent(TreeNode *curNode, TreeNode *newNode)
+{
+    if (curNode->parent)
+    {
+        if (curNode == curNode->parent->leftChild)
+        {
+            curNode->parent->leftChild = newNode;
+        }
+        else
+        {
+            curNode->parent->rightChild = newNode;
+        }
+    }
+
+    if (newNode != NULL)
+    {
+        newNode->parent = curNode->parent;
+    }
+}
+
+void deleteNode(TreeNode* curNode, int data)
+{
+    if (data < treeRoot->key)
+    {
+        deleteNode(treeRoot->leftChild, data);
+    }
+    else if (data > treeRoot->key)
+    {
+        deleteNode(treeRoot->rightChild, data);
+    }
+    //delete the node here
+    else
+    {
+        //if both children are present
+        if (curNode->rightChild != NULL && curNode->leftChild != NULL)
+        {
+            //get the smallest that bigger than curNode
+            TreeNode* successor = getSuccessor(curNode->rightChild);
+            curNode->key = successor->key;
+
+            //if successor has a child, replace it with successor, 
+            //at this point, it can only has a right child.
+            //if it has no child, successor->rightChild will be NONE.
+            replaceNodeInParent(successor, successor->rightChild);
+        }
+        else if (curNode->rightChild != NULL)
+        {
+            replaceNodeInParent(curNode, curNode->rightChild);
+        }
+        else if (curNode->leftChild != NULL)
+        {
+            replaceNodeInParent(curNode, curNode->leftChild);
+        }
+        else
+        {
+            replaceNodeInParent(curNode, NULL);
+        }
+    }
+}
+
+void inOrderTraversal(TreeNode *curNode)
+{
+   if (curNode == NULL)
+       return;
+
+   inOrderTraversal(curNode->leftChild);
+   cout<<curNode->key;
+   inOrderTraversal(curNode->rightChild);
 }
 
 int main()
